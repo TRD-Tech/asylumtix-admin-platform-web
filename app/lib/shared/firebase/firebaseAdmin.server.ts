5cd699c81cb241fee9ebef6024748a51f69c8f7e
Ayoub Taouarda <ayoubtrdone@gmail.com>
2025-01-25 21:47:56 +0100

    Handle google account service key set through env vars

--- app/lib/shared/firebase/firebaseAdmin.server.ts
+++ app/lib/shared/firebase/firebaseAdmin.server.ts
@@ -1,14 +1,23 @@
 import path from "path";
 import admin from "firebase-admin";
 
+import { writeFileSync, existsSync } from "fs";
+
+const secretsFilePath = path.join(
+  process.cwd(),
+  "secrets",
+  "asylumtix-admin-platform-service-account-key.json"
+);
+
+if (!existsSync) {
+  writeFileSync(
+    secretsFilePath,
+    process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT_KEY as string
+  );
+}
+
 export const firebaseAdmin = admin.apps.length
   ? admin.app()
   : admin.initializeApp({
-      credential: admin.credential.cert(
-        path.join(
-          process.cwd(),
-          "secrets",
-          "asylumtix-admin-platform-service-account-key.json"
-        )
-      ),
+      credential: admin.credential.cert(secretsFilePath),
     });
