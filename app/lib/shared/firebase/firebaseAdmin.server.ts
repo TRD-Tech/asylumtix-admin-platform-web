95009419a6b0538b4302dd8b8a14ce6fc9d94d94
Ayoub Taouarda <ayoubtrdone@gmail.com>
2025-01-26 13:12:40 +0100

    Simplify access to the firebase service account key

--- app/lib/shared/firebase/firebaseAdmin.server.ts
+++ app/lib/shared/firebase/firebaseAdmin.server.ts
@@ -1,23 +1,9 @@
-import path from "path";
 import admin from "firebase-admin";
-
-import { writeFileSync, existsSync } from "fs";
-
-const secretsFilePath = path.join(
-  process.cwd(),
-  "secrets",
-  "asylumtix-admin-platform-service-account-key.json"
-);
-
-if (!existsSync) {
-  writeFileSync(
-    secretsFilePath,
-    process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT_KEY as string
-  );
-}
 
 export const firebaseAdmin = admin.apps.length
   ? admin.app()
   : admin.initializeApp({
-      credential: admin.credential.cert(secretsFilePath),
+      credential: admin.credential.cert(
+        JSON.parse(process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT_KEY as string)
+      ),
     });
