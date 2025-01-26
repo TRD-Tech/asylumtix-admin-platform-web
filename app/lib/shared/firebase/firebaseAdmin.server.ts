4d036eaf7dc0d5a782aa7defede8c6051c26eda0
Ayoub Taouarda <ayoubtrdone@gmail.com>
2025-01-26 13:37:28 +0100

    Handle base64 encoded firebase service account keys in env

--- app/lib/shared/firebase/firebaseAdmin.server.ts
+++ app/lib/shared/firebase/firebaseAdmin.server.ts
@@ -1,9 +1,22 @@
 import admin from "firebase-admin";
 
+let serviceAccountKeyJson: string;
+
+if (process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT_KEY_JSON) {
+  serviceAccountKeyJson = process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT_KEY_JSON;
+} else if (process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT_KEY_BASE64) {
+  serviceAccountKeyJson = Buffer.from(
+    process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT_KEY_BASE64,
+    "base64"
+  ).toString("ascii");
+} else {
+  throw new Error(
+    "Missing FIREBASE_ADMIN_SERVICE_ACCOUNT_KEY_JSON or FIREBASE_ADMIN_SERVICE_ACCOUNT_KEY_BASE64 environment variable"
+  );
+}
+
 export const firebaseAdmin = admin.apps.length
   ? admin.app()
   : admin.initializeApp({
-      credential: admin.credential.cert(
-        JSON.parse(process.env.FIREBASE_ADMIN_SERVICE_ACCOUNT_KEY as string)
-      ),
+      credential: admin.credential.cert(JSON.parse(serviceAccountKeyJson)),
     });
